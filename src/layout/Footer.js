1f3d69ddf1aed571c3685428709a772f23aa1071
Emmanuel Ekpobimi <noble4special247@gmail.com>
2023-01-20 17:36:57 +0100

    Added meaningful content to the footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,41 +1,224 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Nav, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Footer() {
   const thisYear = new Date().getFullYear();
 
   return (
     <Container fluid className='footer-background-style text-white pt-5 px-0'>
-      <Container className='pb-5'>
-        <Row>
-          <Col md={4}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
-            similique iure libero iste sunt dignissimos voluptate at dicta
-            obcaecati fugiat, aliquid in maxime, nostrum minima recusandae
-            ducimus ipsa nisi ad?
-          </Col>
-          <Col md={4}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Asperiores, nam aperiam praesentium earum beatae voluptatum deserunt
-            maxime numquam natus unde minus, velit similique quae accusamus vel
-            neque sint illum quam.
-          </Col>
-          <Col md={4}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-            libero numquam reiciendis aliquam dolores maiores quasi, nam omnis
-            dolorum esse explicabo in exercitationem, quae ipsam porro, nobis
-            saepe voluptates veniam?
-          </Col>
-        </Row>
-      </Container>
-      <div className='bg-primary py-3'>
-        <small className=' d-block text-center'>
-          &copy; Copyright {thisYear} Xplorers Computer Training School. All
-          Rights Reserved
-        </small>
-      </div>
+      <small>
+        <Container className='pb-5'>
+          <Row className='justify-content-between'>
+            <Col md={6} lg={3}>
+              <section className='mt-3'>
+                <h2
+                  className='mb-0 font-weight-bolder'
+                  style={{ letterSpacing: "8px" }}
+                >
+                  XPLORERS
+                </h2>
+                <small className='d-block font-weight-bold'>
+                  Computer Training School
+                </small>
+                <p className='mt-2'>
+                  The best way is to develop and follow a plan. Start with your
+                  goals in mind and then work backward to develop the plan.
+                </p>
+                <h3>Follow Us</h3>
+                <div className='d-flex justify-content-start'>
+                  <a
+                    href='/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='btn btn-secondary d-block mr-3'
+                  >
+                    <i class='bi bi-facebook text-white'></i>
+                  </a>
+
+                  <a
+                    href='/'
+                    target='_blank'
+                    className='btn btn-secondary d-block mr-3'
+                  >
+                    <i class='bi bi-twitter text-white'></i>
+                  </a>
+                  <a
+                    href='/'
+                    target='_blank'
+                    className='btn btn-secondary d-block mr-3'
+                  >
+                    <i class='bi bi-linkedin text-white'></i>
+                  </a>
+                  <a
+                    href='/'
+                    target='_blank'
+                    className='btn btn-secondary d-block mr-3'
+                  >
+                    <i class='bi bi-instagram text-white'></i>
+                  </a>
+                </div>
+              </section>
+            </Col>
+            <Col md={6} lg={3}>
+              <section className='mt-3'>
+                <h3>Featured Courses</h3>
+                <Nav className='flex-column'>
+                  <Nav.Link className='px-0'>
+                    <Link to='/' className='text-white'>
+                      ICT Fundamentals
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link className='px-0'>
+                    <Link to='/' className='text-white'>
+                      Database Management
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link className='px-0'>
+                    <Link to='/' className='text-white'>
+                      Web Development
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link className='px-0'>
+                    <Link to='/' className='text-white'>
+                      Computer Repairs & Maintenance
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link className='px-0'>
+                    <Link to='/' className='text-white'>
+                      Graphics & Design
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link className='px-0'>
+                    <Link to='/' className='text-white'>
+                      Computer Networking
+                    </Link>
+                  </Nav.Link>
+                </Nav>
+              </section>
+            </Col>
+            <Col md={6} lg={3}>
+              <section className='mt-3'>
+                <h3>Useful Links</h3>
+                <Nav className='flex-column'>
+                  <Nav.Link className='px-0'>
+                    <Link to='/' className='text-white'>
+                      Home
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link className='px-0'>
+                    <Link to='about-us' className='text-white'>
+                      About Us
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link className='px-0'>
+                    <Link to='training-services' className='text-white'>
+                      Training Services
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link className='px-0'>
+                    <Link to='contact-us' className='text-white'>
+                      Contact Us
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link className='px-0'>
+                    <Link to='faq' className='text-white'>
+                      FAQ
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link className='px-0'>
+                    <Link to='pricing' className='text-white'>
+                      Pricing
+                    </Link>
+                  </Nav.Link>
+                </Nav>
+              </section>
+            </Col>
+            <Col md={6} lg={3}>
+              <section className='mt-3'>
+                <h3>Contact Us</h3>
+                <div className='d-flex'>
+                  <p className='text-secondary font-weight-bold'>
+                    <i class='bi bi-geo-alt-fill'></i>
+                  </p>
+                  <p className='ml-2'>
+                    Suite 208 Cosy Plaza, Ada-George Road, Near Location
+                    Bus-Stop, Mgbuoba Port Harcourt, Rivers State, Nigeria
+                  </p>
+                </div>
+                <div className='d-flex'>
+                  <p className='text-secondary font-weight-bold'>
+                    <i class='bi bi-telephone-inbound-fill'></i>
+                  </p>{" "}
+                  <p className='ml-2'>+2348039356492, +2348037804027</p>
+                </div>
+                <div className='d-flex'>
+                  <p className='text-secondary font-weight-bold'>
+                    <i class='bi bi-envelope-at-fill'></i>
+                  </p>
+                  <p className='ml-2'>info@xplorerstrainingschool.com</p>
+                </div>
+              </section>
+            </Col>
+          </Row>
+        </Container>
+        <div className='bg-primary py-3'>
+          <p className='text-center m-0'>
+            &copy; Copyright {thisYear} Xplorers Computer Training School. All
+            Rights Reserved.
+          </p>
+        </div>
+      </small>
     </Container>
   );
 }
 
 export default Footer;
+
+/*
+<Nav className='mt-3'>
+<h2>Featured Courses</h2>
+<Nav.Item>
+  <Nav.Link>
+    <Link to='/' className='text-white text-decoration-none'>
+      ICT Fundamentals
+    </Link>
+  </Nav.Link>
+</Nav.Item>
+<Nav.Item>
+  <Nav.Link>
+    <Link to='/' className='text-white text-decoration-none'>
+      Database Management
+    </Link>
+  </Nav.Link>
+</Nav.Item>
+<Nav.Item>
+  <Nav.Link>
+    <Link to='/' className='text-white text-decoration-none'>
+      Computer Repairs & Maintenance
+    </Link>
+  </Nav.Link>
+</Nav.Item>
+<Nav.Item>
+  <Nav.Link>
+    <Link to='/' className='text-white text-decoration-none'>
+      Computer Networking
+    </Link>
+  </Nav.Link>
+</Nav.Item>
+<Nav.Item>
+  <Nav.Link>
+    <Link to='/' className='text-white text-decoration-none'>
+      Web Development
+    </Link>
+  </Nav.Link>
+</Nav.Item>
+<Nav.Item>
+  <Nav.Link>
+    <Link to='/' className='text-white text-decoration-none'>
+      Graphics & Design
+    </Link>
+  </Nav.Link>
+</Nav.Item>
+</Nav> 
+*/
