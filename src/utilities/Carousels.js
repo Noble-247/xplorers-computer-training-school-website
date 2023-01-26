d29ec84d5319d538806cb8af454bef474f42efcc
Emmanuel Ekpobimi <noble4special247@gmail.com>
2023-01-26 17:37:39 +0100

    Moved inline css to the index.css file, and made modifications to the animation-caption

--- src/utilities/Carousels.js
+++ src/utilities/Carousels.js
@@ -1,30 +1,39 @@
 import { Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import firstCarouselImage from "../images/carousel-image-1.jpg";
 import secondCarouselImage from "../images/carousel-image-2.jpg";
 import thirdCarouselImage from "../images/carousel-image-3.jpg";
 import fourthCarouselImage from "../images/carousel-image-4.jpg";
 
 function Carousels() {
-  const carouselCaptionBackground = {
-    backgroundColor: "rgba(30, 43, 55, 0.8)",
-  };
-
   return (
     <div className='d-none d-md-block p-0'>
       <Carousel fade>
-        <Carousel.Item>
+        <Carousel.Item className='position-relative'>
           <img
             className='d-block w-100'
             src={firstCarouselImage}
             alt='First slide'
           />
-          <Carousel.Caption className='px-3' style={carouselCaptionBackground}>
-            <h3>ACCELERATING DISCOVERY</h3>
-            <p>
-              Research Journals accelerates the dissemination of knowlege
-              throuth the publication of high quality research articles using
-              the open access model
-            </p>
+          <Carousel.Caption>
+            <div className='carousel-caption-style'>
+              <h1 className='text-center font-weight-bold'>
+                Exceptional ICT Education. <br /> Exceptional Value
+              </h1>
+              <p className='text-center px-5'>
+                First thing to remember about success is that it is a process
+                nothing more, nothing less. There is really no magic to it. it
+                is not reserved for a selected few people{" "}
+              </p>
+              <div className='d-flex'>
+                <Link to='/' className='btn btn-light d-block mr-3 btn-lg'>
+                  Read More
+                </Link>
+                <Link to='/' className='btn btn-secondary d-block btn-lg'>
+                  Learn More
+                </Link>
+              </div>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -33,14 +42,25 @@ function Carousels() {
             src={secondCarouselImage}
             alt='Second slide'
           />
-
-          <Carousel.Caption className='px-3' style={carouselCaptionBackground}>
-            <h3>ACCELERATING DISCOVERY</h3>
-            <p>
-              Research Journals accelerates the dissemination of knowlege
-              throuth the publication of high quality research articles using
-              the open access model
-            </p>
+          <Carousel.Caption>
+            <div className='carousel-caption-style'>
+              <h1 className='text-center font-weight-bold'>
+                Exceptional ICT Education. <br /> Exceptional Value
+              </h1>
+              <p className='text-center px-5'>
+                First thing to remember about success is that it is a process
+                nothing more, nothing less. There is really no magic to it. it
+                is not reserved for a selected few people{" "}
+              </p>
+              <div className='d-flex'>
+                <Link to='/' className='btn btn-light d-block mr-3 btn-lg'>
+                  Read More
+                </Link>
+                <Link to='/' className='btn btn-secondary d-block btn-lg'>
+                  Learn More
+                </Link>
+              </div>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -49,14 +69,25 @@ function Carousels() {
             src={thirdCarouselImage}
             alt='Third slide'
           />
-
-          <Carousel.Caption className='px-3' style={carouselCaptionBackground}>
-            <h3>ACCELERATING DISCOVERY</h3>
-            <p>
-              Research Journals accelerates the dissemination of knowlege
-              throuth the publication of high quality research articles using
-              the open access model
-            </p>
+          <Carousel.Caption>
+            <div className='carousel-caption-style'>
+              <h1 className='text-center font-weight-bold'>
+                Exceptional ICT Education. <br /> Exceptional Value
+              </h1>
+              <p className='text-center px-5'>
+                First thing to remember about success is that it is a process
+                nothing more, nothing less. There is really no magic to it. it
+                is not reserved for a selected few people{" "}
+              </p>
+              <div className='d-flex'>
+                <Link to='/' className='btn btn-light d-block mr-3 btn-lg'>
+                  Read More
+                </Link>
+                <Link to='/' className='btn btn-secondary d-block btn-lg'>
+                  Learn More
+                </Link>
+              </div>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -65,14 +96,25 @@ function Carousels() {
             src={fourthCarouselImage}
             alt='Third slide'
           />
-
-          <Carousel.Caption className='px-3' style={carouselCaptionBackground}>
-            <h3>ACCELERATING DISCOVERY</h3>
-            <p>
-              Research Journals accelerates the dissemination of knowlege
-              throuth the publication of high quality research articles using
-              the open access model
-            </p>
+          <Carousel.Caption>
+            <div className='carousel-caption-style'>
+              <h1 className='text-center font-weight-bold'>
+                Exceptional ICT Education. <br /> Exceptional Value
+              </h1>
+              <p className='text-center px-5'>
+                First thing to remember about success is that it is a process
+                nothing more, nothing less. There is really no magic to it. it
+                is not reserved for a selected few people{" "}
+              </p>
+              <div className='d-flex'>
+                <Link to='/' className='btn btn-light d-block mr-3 btn-lg'>
+                  Read More
+                </Link>
+                <Link to='/' className='btn btn-secondary d-block btn-lg'>
+                  Learn More
+                </Link>
+              </div>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
@@ -81,3 +123,26 @@ function Carousels() {
 }
 
 export default Carousels;
+
+/* 
+
+<div style={carouselTextStyle}>
+            <h1 className='text-center display-4 font-weight-bold'>
+              Exceptional ICT Education. <br /> Exceptional Value
+            </h1>
+            <p className='text-center px-5'>
+              First thing to remember about success is that it is a process
+              nothing more, nothing less. There is really no magic to it. it is
+              not reserved for a selected few people{" "}
+            </p>
+            <div className='d-flex'>
+              <Link to='/' className='btn btn-light d-block mr-3 btn-lg'>
+                Read More
+              </Link>
+              <Link to='/' className='btn btn-secondary d-block btn-lg'>
+                Learn More
+              </Link>
+            </div>
+          </div>
+
+*/
